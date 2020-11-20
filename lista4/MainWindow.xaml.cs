c44495d099d1189eb5fd7c58d4da87b1856a4263
Dexter1300 <127375@student.uni.opole.pl>
2020-11-20 18:13:21 +0100

    Dodanie klasy Student

--- lista4/MainWindow.xaml.cs
+++ lista4/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Serialization;
 
 namespace lista4
 {
@@ -23,6 +24,47 @@ namespace lista4
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public class Student
+        {
+            [XmlAttribute("id")]
+            public int StudentId { get; set; }
+
+            [XmlAttribute("imie")]
+            public string FirstName { get; set; }
+
+            [XmlAttribute("nazwisko")]
+            public string LastName { get; set; }
+
+            [XmlAttribute("wiek")]
+            public int Age { get; set; }
+
+            [XmlAttribute("pesel")]
+            public long Pesel { get; set; }
+
+            [XmlElement("Obraz")]
+            public string obraz { get; set; }
+
+            public Student(int nStudentId, string sFirstName, string sLastName, int nAge, long lPesel, string Obraz)
+            {
+                StudentId = nStudentId;
+                FirstName = sFirstName;
+                LastName = sLastName;
+                Age = nAge;
+                Pesel = lPesel;
+                obraz = Obraz;
+            }
+
+            public Student()
+            {
+                StudentId = 0;
+                FirstName = "Janusz";
+                LastName = "Januszewski";
+                Age = 120;
+                Pesel = 999909090;
+                obraz = "image";
+            }
         }
     }
 }
